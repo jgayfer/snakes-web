f4fea1aace94556a0103b4354bee68fce2c53189
James Gayfer <gayfer.james@gmail.com>
2018-02-26 14:08:21 -0800

    Add basic routing structure and logic

--- app.rb
+++ app.rb
@@ -1,10 +1,55 @@
 require 'snakes'
 require 'roda'
+require 'pstore'
 
 class App < Roda
+  plugin :all_verbs
+  plugin :error_handler do |e|
+    "Error: #{e.message}"
+  end
+
   route do |r|
-    r.root do
-      'test'
+    r.on 'game' do
+
+      # POST /game
+      r.post do
+        new_game = Snakes.standard_game(r.params['players'].split(','))
+        save_game(new_game)
+        'post' # game json
+      end
+
+      r.is Integer do |id|
+        game = find_game(id)
+
+        # GET /game/{id}
+        r.get do
+          'get' # game json
+        end
+
+        # PUT /game/{id}
+        r.put do
+          game.move_next_player if r.params['move_next_player']
+          save_game(game)
+          'put' # game json
+        end
+      end
     end
+  end
+
+  def save_game(game)
+    store = db
+    store.transaction do
+      store[game.object_id] = game
+      store.commit
+    end
+  end
+
+  def find_game(id)
+    store = db
+    store.transaction { store[id] }
+  end
+
+  def db
+    PStore.new('my_database.pstore')
   end
 end
