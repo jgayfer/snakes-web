ca9fd30904277b44822a6fb61f405a8a4b6e35e5
James Gayfer <james@stembolt.com>
2018-03-05 10:57:50 -0800

    Add ServerGame class
    The server game will serve as the master of all information regarding a
    game and its connected clients. This will ensure that when a client
    tries to modify the state of a game, we will be able to determine
    whether or not they are allowed to do so.

---
+++ spec/server_game_spec.rb
@@ -0,0 +1,54 @@
+require 'server_game'
+require 'client'
+require 'snakes'
+require 'securerandom'
+
+RSpec.describe ServerGame do
+  let(:game) { Snakes.standard_game([]) }
+  let(:client) { Client.new(Snakes::Player.new('dummy'), '123abc') }
+  let(:client2) { Client.new(Snakes::Player.new('dummy2'), '123abc') }
+  let(:server_game) { ServerGame.new(game, '123abc') }
+
+  describe '#initialize' do
+    subject { server_game }
+    it { is_expected.to be_a ServerGame }
+  end
+
+  describe '#add_client' do
+    subject { server_game }
+    before { server_game.add_client(client) }
+
+    it 'added a new client' do
+      expect(subject.clients.first).to be_a Client
+      expect(subject.game.previous_player).to eq(client.player)
+    end
+  end
+
+  describe '#client_id_in_game?' do
+    subject { server_game.client_id_in_game?(client.id) }
+
+    context 'client in game' do
+      before { server_game.add_client(client) }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'client not in game' do
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '#client_id_is_next_player?' do
+    subject { server_game.client_id_is_next_player?(client.id) }
+    before { server_game.add_client(client) }
+    before { server_game.add_client(client2) }
+
+    context 'client is next player' do
+      it { is_expected.to be_truthy }
+    end
+
+    context 'client is not next player' do
+      before { server_game.game.move_next_player }
+      it { is_expected.to be_falsey }
+    end
+  end
+end
