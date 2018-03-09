51aa6da701510c5f4909e86b26f67018c6b19480
James Gayfer <james@stembolt.com>
2018-03-09 14:24:56 -0800

    Add test assertions for response bodies

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,10 +7,14 @@ describe 'Snakes API routes' do
   let(:game_id) { 'game-id' }
   let(:client1_id) { 'client1-id' }
   let(:client2_id) { 'client2-id' }
+  let(:player1_name) { 'player1' }
+  let(:player2_name) { 'player2' }
+  let(:player1) { Snakes::Player.new(player1_name) }
+  let(:player2) { Snakes::Player.new(player2_name) }
+  let(:client1) { SnakesAPI::Client.new(player1, client1_id) }
+  let(:client2) { SnakesAPI::Client.new(player2, client2_id) }
   let(:game) { Snakes.standard_game([]) }
   let(:server_game) { SnakesAPI::ServerGame.new(game, game_id) }
-  let(:client1) { SnakesAPI::Client.new(Snakes::Player.new('player1'), client1_id) }
-  let(:client2) { SnakesAPI::Client.new(Snakes::Player.new('player2'), client2_id) }
   before { server_game.add_client(client1) }
   before { server_game.add_client(client2) }
 
@@ -26,6 +30,7 @@ describe 'Snakes API routes' do
     it 'retrieves an existing game' do
       get "/game/#{game_id}/?client_id=#{client1_id}"
       expect(last_response).to be_ok
+      expect(last_response.body).to eq(fixture('get_game.json').chomp)
     end
 
     it 'returns an error if no game exists' do
@@ -38,6 +43,7 @@ describe 'Snakes API routes' do
     it 'creates a new game' do
       post '/game/?player=name1'
       expect(last_response).to be_ok
+      expect(last_response.body).to eq(fixture('create_game.json').chomp)
     end
 
     it 'returns an error if no player is given' do
@@ -50,6 +56,7 @@ describe 'Snakes API routes' do
     it 'moves the next player in a game' do
       post "/game/#{game_id}/move/?client_id=#{client1_id}"
       expect(last_response).to be_ok
+      expect(last_response.body).to eq('{}')
     end
 
     it 'returns an error if not part of game' do
@@ -73,6 +80,7 @@ describe 'Snakes API routes' do
     it 'joins an existing game' do
       post "/game/#{game_id}/join/?player=someone"
       expect(last_response).to be_ok
+      expect(last_response.body).to eq(fixture('join_game.json').chomp)
     end
 
     it 'returns an error if no player is given' do
@@ -85,6 +93,7 @@ describe 'Snakes API routes' do
     it 'starts the game' do
       post "/game/#{game_id}/start?client_id=#{client1_id}"
       expect(last_response).to be_ok
+      expect(last_response.body).to eq('{}')
     end
 
     it 'returns an error if no client id is given' do
